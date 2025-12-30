01a1c616b3a89f6105630a2c9502594b39166bcc
Tamura Yuji <uny.nippon@gmail.com>
2025-12-30 21:41:23 +0900

    Add initial project structure and resources for SimpleCounter app

---
+++ SimpleCounter/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleCounter;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        Window window = new Window(new AppShell());
+        window.Height = 600;
+        window.Width = 400;
+
+        return window;
+    }
+}
---
+++ SimpleCounter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleCounter;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
